459d851e6b96976e2bc86f234c0da9f36022fb3e
naddola <jeonhyeoncheol@Jeon-HyeonCheol-ui-MacBook-Air.local>
2014-06-12 12:31:37 +0900

    140612 Just merge two Project

--- naddola/Give2Gether/src/com/example/give2gether/MainActivity.java
+++ naddola/Give2Gether/src/com/example/give2gether/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Intent intent = new Intent(MainActivity.this, SignUpSelect.class);
+		Intent intent = new Intent(MainActivity.this, SignUpSelectActivity.class);
 		startActivity(intent);
 	}
 	
--- woosik/Giv2gether/src/com/example/giv2gether/StartActivity.java
+++ woosik/Giv2gether/src/com/example/giv2gether/StartActivity.java
@@ -1,5 +1,48 @@
 package com.example.giv2gether;
 
-public class StartActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class StartActivity extends Activity implements OnClickListener{
+	
+	Button bt_Signup;
+	Button bt_Login;
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start);
+		
+		init();
+	}
+	
+	public void init(){
+		bt_Signup = (Button)findViewById(R.id.start_signup_bt);
+		bt_Login = (Button)findViewById(R.id.start_login_bt);
+		
+		bt_Signup.setOnClickListener(this);
+		bt_Login.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+		Intent intent;
+		
+		switch(v.getId()){
+		
+		case R.id.start_signup_bt:
+			intent = new Intent(StartActivity.this, SignUpSelectActivity.class);
+			startActivity(intent);
+			break;
+			
+		case R.id.start_login_bt:
+			break;
+		}
+		
+	}
 
 }
