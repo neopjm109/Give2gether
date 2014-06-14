6ce2049fd7fabe70cbb7a0462c5e993aaa7e73ac
naddola <jeonhyeoncheol@Jeon-HyeonCheol-ui-MacBook-Air.local>
2014-06-14 14:09:19 +0900

    140614 first Success AutoLogin

--- woosik/Giv2gether/src/com/example/giv2gether/MainFragment.java
+++ woosik/Giv2gether/src/com/example/giv2gether/MainFragment.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.TextView;
 
 public class MainFragment extends Fragment {
 
@@ -17,7 +21,24 @@ public class MainFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		rootView = inflater.inflate(R.layout.tab_main, container, false);
-				
+		
+		final SettingPreference setting = new SettingPreference(getActivity());
+		
+		TextView tv = (TextView)rootView.findViewById(R.id.mText);
+		tv.setText("welcome " + setting.getID());
+		CheckBox ck = (CheckBox)rootView.findViewById(R.id.Setting_AutoLogin);
+		ck.setChecked(setting.isLogin());
+		ck.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if(isChecked)
+					setting.setAutoLoginTrue();
+				else
+					setting.setAutoLoginFalse();
+			}
+		});
+		
 		return rootView;
 	}
 }
